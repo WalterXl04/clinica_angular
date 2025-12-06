8d234f37c6a269f7a3b6abebf9df66bef2c04255
Marcus Vinicius Meda de Castro <luis.meda@aluno.imepac.edu.br>
2025-12-05 22:25:42 -0300

    Organizando_angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+import { Routes } from '@angular/router';
+import { Home } from './pages/public/home/home';
+
+
+export const routes: Routes = [
+ { path: '', component: Home },     // Home como principal
+  { path: 'home', component: Home }  // Rota opcional /home
+];
---
+++ src/app/pages/private/administrativo/administrativo.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-administrativo',
+  imports: [],
+  templateUrl: './administrativo.html',
+  styleUrl: './administrativo.css',
+})
+export class Administrativo {
+
+}
