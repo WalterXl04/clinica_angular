4c7ab26912bd6d739430a1ffde0be0d8d45dbcfd
gedersson7 <gederssonbuchini@gmail.com>
2025-12-06 17:49:50 -0300

    Pagina-Home

--- src/app/pages/public/home/home.ts
+++ src/app/pages/public/home/home.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Navbar } from '../../../components/navbar/navbar';
+import { Footer } from '../../../components/footer/footer';
 
 @Component({
   standalone: true,
   selector: 'app-home',
-  imports: [CommonModule],
+  imports: [CommonModule, Navbar, Footer],
   templateUrl: './home.html',
   styleUrls: ['./home.css'],
 })
