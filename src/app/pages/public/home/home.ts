7c5966b62b9e7a036bbc3b3dd60a414612d1008e
Marcus Vinicius Meda de Castro <luis.meda@aluno.imepac.edu.br>
2025-12-06 19:45:25 -0300

    mais um

--- src/app/pages/public/home/home.ts
+++ src/app/pages/public/home/home.ts
@@ -1,13 +1,45 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Navbar } from '../../../components/navbar/navbar';
-import { Footer } from '../../../components/footer/footer';
+import { NavbarComponent } from '../../../components/navbar/navbar';
+import {  FooterComponent } from '../../../components/footer/footer';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   standalone: true,
   selector: 'app-home',
-  imports: [CommonModule, Navbar, Footer],
+  imports: [CommonModule, NavbarComponent , FooterComponent, FormsModule],
   templateUrl: './home.html',
   styleUrls: ['./home.css'],
 })
-export class Home {}
+export class Home {
+  contato = {
+    nome: '',
+    email: '',
+    telefone: '',
+    assunto: '',
+    mensagem: ''
+  };
+
+  // Variável para controlar se o formulário foi enviado
+  enviado = false;
+
+  enviarContato() {
+    // Aqui você poderia enviar os dados para uma API
+    console.log('Contato enviado:', this.contato);
+
+    // Marcar como enviado
+    this.enviado = true;
+
+    // Limpar os dados do formulário (opcional)
+    this.contato = {
+      nome: '',
+      email: '',
+      telefone: '',
+      assunto: '',
+      mensagem: ''
+    };
+
+    // Exibir pop-up
+    alert('Mensagem enviada com sucesso! Entraremos em contato em breve.');
+  }
+}
